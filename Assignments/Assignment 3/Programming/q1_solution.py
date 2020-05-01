6b0c347791f20b72e261a7390f7b90fa07ae4a03
Shima Shahfar <shimashahfar@gmail.com>
2020-05-01 19:07:12 -0400

    add assignment 3

--- Assignments/Assignment 3/Programming/q1_solution.py
+++ Assignments/Assignment 3/Programming/q1_solution.py
@@ -23,8 +23,8 @@ def log_likelihood_bernoulli(mu, target):
     target = target.view(batch_size, -1)
 
     # log_likelihood_bernoulli
-    
-    return 
+    ll = (target * torch.log(mu)) + ((1 - target) * torch.log(1 - mu))
+    return ll.sum(dim=1)
 
 
 def log_likelihood_normal(mu, logvar, z):
@@ -45,8 +45,8 @@ def log_likelihood_normal(mu, logvar, z):
     z = z.view(batch_size, -1)
 
     # log normal
-    
-    return 
+    ll = -((logvar + np.log(2 * np.pi)) + ((z - mu) ** 2) / logvar.exp()) / 2
+    return ll.sum(dim=1)
 
 
 def log_mean_exp(y):
